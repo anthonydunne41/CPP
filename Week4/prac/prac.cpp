1efa5fc5c3b7e7b86acf4d577725ff40eb03f98b
Anthony <dunap001@mymail.unisa.com.au>
2022-10-16 20:49:12 +1030

    poop

--- Week4/prac/prac.cpp
+++ Week4/prac/prac.cpp
@@ -2,34 +2,59 @@
 #include <string>
 #include <set>
 #include <cmath>
-
-/*
-Write an application that prompts the user for a set of numbers (integers) and stores them in a 
-std::set. You may ask the user for the number of values that they will enter or use some other 
-means to end the set of numbers. 
-• Refer to the lecture material and cppreference 
-(https://en.cppreference.com/w/cpp/container/set/set) if you are having difficulty with the 
-concepts. 
-• Your application should not crash or enter an infinite loop if the user enters an invalid value. 
-Be sure to follow good practice and validate the inputs: display an error and re-prompt if an 
-invalid value is provided. 
-*/
+#include <map>
 
 int main()
 {
-	std::cout << "How many numbers? \n";
-	int i{};
-	std::cin >> i;
-	std::set<int> numbers;
-	for (int a = 0; a < i; a++)
+	int value_length;
+	int number;
+	std::set<int> values;
+	std::map<int, std::string> value_map;
+
+	std::cout << "How many numbers do you want to give me? \n";
+
+	std::cin >> value_length;
+	for (int i = 0; i < value_length; i++)
 	{
-		std::cout << "enter number:  \n";
-		int b;
-		std::cin >> b;
-		numbers.insert(b);
+		std::cout << "Enter number: ";
+		std::cin >> number;
+		while (values.count(number))
+		{
+			std::cout << "Number already in set, try again: ";
+			std::cin >> number;
+		}
+		values.insert(number);
 	}
 
+	std::cout << "Your numbers : ";
+	for (auto a : values)
+	{
+		std::cout << a << " ";
+	}
 
+	std::cout << "\nGive me a number to erase: ";
+	std::cin >> number;
+
+	if (values.count(number))
+	{
+		values.erase(number);
+	}
+
+	std::cout << "Your numbers : ";
+	for (auto a : values)
+	{
+		std::cout << a << " ";
+	}
+	std::cout << '\n';
+
+	std::string ass_string;
+	for (auto a : values)
+	{
+		std::cout << "Enter string to be associated with " << a << ": \n";
+		std::cin >> ass_string;
+		value_map[a] = ass_string;
+	}
+	
 
 
 
